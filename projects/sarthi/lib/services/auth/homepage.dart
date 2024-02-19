689a2c1e3e4503fa49a368818bb94062005b0b14
Tejaswi Jadhav <94731975+tejaswijadhav2003@users.noreply.github.com>
2024-02-20 03:31:00 +0530

    added named parameter

--- projects/sarthi/lib/services/auth/homepage.dart
+++ projects/sarthi/lib/services/auth/homepage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({super.key, required String title});
 
   @override
   State<HomePage> createState() => _HomePageState();
