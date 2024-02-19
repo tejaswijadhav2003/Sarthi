1fbcd5242ef584ccdc241dfd82c559b79a73c5a6
Tejaswi Jadhav <94731975+tejaswijadhav2003@users.noreply.github.com>
2024-02-20 03:33:28 +0530

    verification id added

--- projects/sarthi/lib/login_page.dart
+++ projects/sarthi/lib/login_page.dart
@@ -75,7 +75,7 @@ class _LoginPageState extends State<LoginPage> {
                               Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => OtpPage()));
+                                      builder: (context) => OtpPage(verificationId: '',)));
                             },
                             codeAutoRetrievalTimeout:
                                 (String verificationId) {},
